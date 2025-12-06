83029e122693196e9810870f1a7d02285c83ddc8
jarbelaezu98 <jarbelaezu@unal.edu.co>
2025-12-06 11:43:33 -0500

    Gitignore modificado

--- make_predictions.py
+++ make_predictions.py
@@ -20,7 +20,7 @@ x = df.drop(columns=["quality"])
 ## Debe verificarse el run_id del modelo que se quiere cargar
 ## Se puede obtener el run_id desde la interfaz de MLflow
 
-logged_model = "runs:/fffcc19ab7e14260b7bd424c9e0da014/model"
+logged_model = "runs:/a5a7f796de744874abfb43a5756c555c/model"
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 y = loaded_model.predict(x)
 
